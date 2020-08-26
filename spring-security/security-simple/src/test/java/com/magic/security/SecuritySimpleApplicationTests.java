129cc79a51dffecc6528ec58f722f45cb67929d4
magiclz233 <magiclz233@163.com>
2020-08-26 23:55:23 +0800

    spring security

---
+++ spring-security/security-simple/src/test/java/com/magic/security/SecuritySimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
