1f804556de51076d5830c6e87a62869abcace995
magiclz233 <magiclz233@163.com>
2020-08-28 00:10:10 +0800

    JwtUtil

--- spring-security/security-simple/src/main/java/com/magic/security/util/JwtUtil.java
+++ spring-security/security-simple/src/main/java/com/magic/security/util/JwtUtil.java
@@ -1,14 +1,18 @@
 package com.magic.security.util;
 
 import com.magic.security.constants.SecurityConstants;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.crypto.SecretKey;
 import javax.xml.bind.DatatypeConverter;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author magic_lz
@@ -49,6 +53,33 @@ public class JwtUtil {
                 .setExpiration(expirationDate)
                 .compact();
         return SecurityConstants.TOKEN_PREFIX + tokenPrefix;
+    }
+
+    public static boolean isTokenExpired(String token){
+        Date expiredDate = getTokenBody(token).getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+    public static String getUsernameByToken(String token){
+        return getTokenBody(token).getSubject();
+    }
+
+    /**
+     * @deprecated 弃用。改为从数据库中获取，保证权限的即时性。
+     */
+    @Deprecated
+    public static List<SimpleGrantedAuthority> getUserRolesByToken(String token){
+        String role = (String) getTokenBody(token).get(SecurityConstants.ROLE_CLAIMS);
+        return Arrays.stream(role.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    private static Claims getTokenBody(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 
