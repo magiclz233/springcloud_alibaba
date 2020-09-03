a1af121f539434a3aa13e198cf1ea5938b71360c
magic <magiclz233@163.com>
2020-09-03 17:42:06 +0800

    自定义权限认证错误

---
+++ spring-security/security-simple/src/main/java/com/magic/security/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.magic.security.exception;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname JwtAuthenticationEntryPoint
+ * @date 2020/9/3 : 16:13
+ */
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    /**
+     * 当用户尝试访问需要权限才能的REST资源而不提供Token或者Token错误或者过期时，
+     * 将调用此方法发送401响应以及错误信息
+     */
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
