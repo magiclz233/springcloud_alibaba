d43d60572b9ba16b853efe8e18fb83c5b45c6023
magic <magiclz233@163.com>
2020-09-03 17:40:32 +0800

    CorsConfig 跨域设置

---
+++ spring-security/security-simple/src/main/java/com/magic/security/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.magic.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ * @author magic_lz
+ * @version 1.0
+ * @classname CorsConfig
+ * @date 2020/9/3 : 16:53
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                //暴露header中的其他属性给客户端应用程序
+                //如果不设置这个属性前端无法通过response header获取到Authorization也就是token
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600);
+    }
+}
