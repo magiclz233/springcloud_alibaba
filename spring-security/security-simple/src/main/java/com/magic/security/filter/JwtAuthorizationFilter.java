960545b22f88efddf96506007b7c4b884ae416e6
magic <magiclz233@163.com>
2020-09-03 17:41:25 +0800

    权限认证 filter实现

--- spring-security/security-simple/src/main/java/com/magic/security/filter/JwtAuthorizationFilter.java
+++ spring-security/security-simple/src/main/java/com/magic/security/filter/JwtAuthorizationFilter.java
@@ -1,8 +1,26 @@
 package com.magic.security.filter;
 
+import com.magic.security.constants.SecurityConstants;
+import com.magic.security.service.UserDetailsServiceImpl;
+import com.magic.security.util.JwtUtil;
+import com.magic.system.exception.UserNameNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * 过滤器处理所有请求, 并检查是否存在带有正确令牌的Authorization标志
@@ -14,9 +32,46 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Slf4j
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
+    private final UserDetailsServiceImpl userDetailsService;
+    private static final Logger logger = Logger.getLogger(JwtAuthorizationFilter.class.getName());
 
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserDetailsServiceImpl userDetailsService) {
         super(authenticationManager);
+        this.userDetailsService = userDetailsService;
     }
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+        String token = request.getHeader(SecurityConstants.TOKEN_HEADER);
+        if(token == null || !token.startsWith(SecurityConstants.TOKEN_PREFIX)){
+            SecurityContextHolder.clearContext();
+        }else {
+            UsernamePasswordAuthenticationToken authentication = getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * 获取用户认证信息 Authentication
+     */
+    private UsernamePasswordAuthenticationToken getAuthentication(String authorization) {
+        log.info("get authentication");
+        String token = authorization.replace(SecurityConstants.TOKEN_PREFIX, "");
+        try {
+            String username = JwtUtil.getUsernameByToken(token);
+            logger.info("checking username:" + username);
+            if (!StringUtils.isEmpty(username)) {
+                // 这里我们是又从数据库拿了一遍,避免用户的角色信息有变
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+                return userDetails.isEnabled() ? usernamePasswordAuthenticationToken : null;
+            }
+        } catch (UserNameNotFoundException | SignatureException | ExpiredJwtException | MalformedJwtException | IllegalArgumentException exception) {
+            logger.warning("Request to parse JWT with invalid signature . Detail : " + exception.getMessage());
+        }
+        return null;
+    }
 }
