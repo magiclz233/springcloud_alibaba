512a9a42f4c564772811eb91baa3a05780b1d485
magic <magiclz233@163.com>
2020-09-03 17:40:12 +0800

    AuditSecurityConfig 审计功能配置类

---
+++ spring-security/security-simple/src/main/java/com/magic/security/config/AuditSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.magic.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+/**
+ * 审计功能配置类
+ * @author magic_lz
+ * @version 1.0
+ * @classname AuditSecurityConfig
+ * @date 2020/9/3 : 16:53
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditSecurityConfig {
+    @Bean
+    AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
+    }
+}
+
