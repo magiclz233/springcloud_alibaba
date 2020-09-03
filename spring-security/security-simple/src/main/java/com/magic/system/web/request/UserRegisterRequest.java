6fe3069483c2912f594bb19cc7abd9664de8eff4
magic <magiclz233@163.com>
2020-09-03 16:02:24 +0800

    request user vo

---
+++ spring-security/security-simple/src/main/java/com/magic/system/web/request/UserRegisterRequest.java
@@ -0,0 +1,32 @@
+package com.magic.system.web.request;
+
+
+import com.magic.system.entity.User;
+import com.magic.system.validator.FullName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author shuang.kou
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterRequest {
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String password;
+    @FullName
+    @NotBlank
+    private String fullName;
+
+    public User toUser() {
+        return User.builder().fullName(this.getFullName())
+                .userName(this.getUserName())
+                .enabled(true).build();
+    }
+}
