15267b713fff8d7211bd7ab840aa35132c2fd0ec
magic <magiclz233@163.com>
2020-09-03 17:41:48 +0800

    spring security config 实现

--- spring-security/security-simple/src/main/java/com/magic/security/config/SecurityConfig.java
+++ spring-security/security-simple/src/main/java/com/magic/security/config/SecurityConfig.java
@@ -1,10 +1,21 @@
 package com.magic.security.config;
 
+import com.magic.security.exception.JwtAccessDeniedHandler;
+import com.magic.security.exception.JwtAuthenticationEntryPoint;
+import com.magic.security.filter.JwtAuthenticationFilter;
+import com.magic.security.filter.JwtAuthorizationFilter;
+import com.magic.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author magic_lz
@@ -15,9 +26,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 //开启Security
 @EnableWebSecurity
 // 开启注解配置支持
-@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService createUserDetailsService() {
+        return userDetailsServiceImpl;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 设置自定义的userDetailsService 以及 密码编码器
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(bCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,8 +55,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // security 默认csrf是开启的，使用token形式的话，这个就可以关了
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/auth/login").permitAll()
+                // 指定路径下的资源需要验证了的用户才能访问
+                .antMatchers("/api/**").authenticated()
+                .antMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN")
+                // 其他都放行
                 .anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                // 添加自定义filter
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userDetailsServiceImpl))
+                // 不需要session (不创建会话)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 授权异常处理
+                .exceptionHandling().authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                .accessDeniedHandler(new JwtAccessDeniedHandler());
+        // 防止H2 web 页面的Frame 被拦截
+        http.headers().frameOptions().disable();
     }
 }
--- spring-security/security-simple/src/main/java/com/magic/security/service/UserDetailsServiceImpl.java
+++ spring-security/security-simple/src/main/java/com/magic/security/service/UserDetailsServiceImpl.java
@@ -1,18 +1,28 @@
 package com.magic.security.service;
 
+import com.magic.security.entity.JwtUser;
+import com.magic.system.entity.User;
+import com.magic.system.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
- * @author magic_lz
- * @version 1.0
- * @date 2020/9/2 22:34
+ * @author magic
  */
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String name) {
+        User user = userService.find(name);
+        return new JwtUser(user);
+    }
+
 }
