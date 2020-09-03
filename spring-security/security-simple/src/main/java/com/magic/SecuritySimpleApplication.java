4a19feb19ccbface0a142cdb7c46f1a876e84b45
magic <magiclz233@163.com>
2020-09-03 17:40:55 +0800

    Application 位置修改

---
+++ spring-security/security-simple/src/main/java/com/magic/SecuritySimpleApplication.java
@@ -0,0 +1,16 @@
+package com.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author magic
+ */
+@SpringBootApplication
+public class SecuritySimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySimpleApplication.class, args);
+    }
+
+}
