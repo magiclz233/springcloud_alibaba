ef97bc144a6f4ba4e7c9a4295d904d7996a1966f
magic <magiclz233@163.com>
2020-09-09 17:15:22 +0800

    UserDetails扩展类实现

---
+++ spring-security/oauth2-auth-server/src/main/java/com/magic/vo/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package com.magic.vo;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname UserDetails
+ * @date 2020/9/9 : 17:09
+ */
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private String test;
+
+    private boolean isEnable;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnable;
+    }
+}
