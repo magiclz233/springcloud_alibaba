f2cbb26ba14fba7ac87330c000fabf631a56870f
magic <magiclz233@163.com>
2020-09-09 17:14:33 +0800

    RedisConstant 添加

---
+++ spring-security/oauth2-auth-server/src/main/java/com/magic/constant/RedisConstant.java
@@ -0,0 +1,11 @@
+package com.magic.constant;
+
+/**
+ * @author magic_lz
+ * @version 1.0
+ * @classname RedisConstant
+ * @date 2020/9/9 : 16:47
+ */
+public class RedisConstant {
+    public static final String SMS_CODE_PREFIX = "sms:code:";
+}
