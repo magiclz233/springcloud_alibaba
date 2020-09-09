d419b3e86da253410e396a13039d6713724161a5
magic <magiclz233@163.com>
2020-09-09 17:13:38 +0800

    test 测试生成加密后密码方法

--- spring-security/oauth2-auth-server/src/test/java/com/magic/AppTest.java
+++ spring-security/oauth2-auth-server/src/test/java/com/magic/AppTest.java
@@ -2,7 +2,9 @@ package com.magic;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,8 +13,16 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void passwordEncoder(){
+        System.out.println(passwordEncoder.encode("123456"));
     }
 }
