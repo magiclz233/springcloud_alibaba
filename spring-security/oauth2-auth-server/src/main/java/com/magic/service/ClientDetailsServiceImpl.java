1724aca2921fd7a1f69302e0e7590dbd2ed0d30d
magiclz233 <magiclz233@163.com>
2020-09-16 21:44:49 +0800

    ClientDetailsServiceImpl 删除多余导包

--- spring-security/oauth2-auth-server/src/main/java/com/magic/service/ClientDetailsServiceImpl.java
+++ spring-security/oauth2-auth-server/src/main/java/com/magic/service/ClientDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.magic.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
