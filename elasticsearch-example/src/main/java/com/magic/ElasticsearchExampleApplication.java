b4d5f9365dc00f5505bfb46ac3b189fa46ec2629
magic <magiclz233@163.com>
2021-02-07 17:37:15 +0800

    ElasticSearch example create

---
+++ elasticsearch-example/src/main/java/com/magic/ElasticsearchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchExampleApplication.class, args);
+	}
+
+}
